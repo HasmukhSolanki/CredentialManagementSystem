519a35858b571dfb9124116fa6006151b60a3de6
HasmukhSolanki <40667204+HasmukhSolanki@users.noreply.github.com>
2018-06-29 19:43:23 +0530

    CredentialManagementSystem v2

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
-    mongoose = require("mongoose");
+    mongoose = require("mongoose"),
+    validator = require("email-validator"),
+    jwt = require("jsonwebtoken"),
+    bcrypt = require("bcryptjs")
 
-mongoose.connect("mongodb://localhost/Project_sa");
+mongoose.connect("mongodb://localhost/CredentialManagementSystem");
 var dbuser = new mongoose.Schema({
     username: String,
     password: String,
@@ -11,79 +14,156 @@ var dbuser = new mongoose.Schema({
     lastname: String
 });
 
-var dbproject = new mongoose.Schema({
+var ProjectModel = new mongoose.Schema({
     username: String,
-    projectname : String,
-    description : String,
-    shared : String
+    projectname: String,
+    description: String,
+    shared: String
 })
-var dbproject = mongoose.model("ProjectData",dbproject)
-var dbuserobject = mongoose.model("UserData", dbuser)
+
+var SecretCode = "This_is_secret_message_for_login";
+
+var ProjectModel = mongoose.model("ProjectData", ProjectModel)
+var UserModel = mongoose.model("UserData", dbuser)
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
 
 //-------------Login------------------//
 app.post("/login", function (req, res) {
     var username = req.body.username;
     var password = req.body.password;
 
-    dbuserobject.findOne({username : username},function(err , dbresponse){
-        if(dbresponse){
-            res.send("Welcome to Homepage " + dbresponse.firstname+dbresponse.lastname);
+    UserModel.findOne({ username: username }, function (err, modelResponse) {
+        if (password == modelResponse.password) {
+
+            var token = jwt.sign({ id: modelResponse._id }, SecretCode, {
+                expiresIn: 86400 // expires in 24 hours
+            })
+            res.json({
+                username : "Welcome " + modelResponse.username,
+                token: token,                 
+            });
         }
-        else{
+        else {
             res.send("Invalid Details pls try again");
         }
     })
-
 })
 
 //-------------Signup------------------//
 
 app.post("/signup", function (req, res) {
+
+
+
     var username = req.body.username;
     var password = req.body.password;
     var firstname = req.body.firstname;
     var lastname = req.body.lastname;
 
-    dbuserobject.findOne({ username: username }, function (err, output) {
-        if (output) {
-            console.log("old user go to login page")
-            res.send(output + "old user go to login page");
-        }
-        else {
-            console.log("new user");
+    if (validator.validate(username)) {
 
-            var userdata = { username: username, password: password, firstname: firstname, lastname: lastname }
-            dbuserobject.create(userdata, function (err, dbresponse) {
-                if (err) {
-                    res.send(err);
-                }
-                else {
-                    res.send(dbresponse);
-                }
-            })
-        }
+        UserModel.findOne({ username: username }, function (err, output) {
+            if (output) {
+                res.send("old user go to login page");
+            }
+            else {
+                var userdata = { username: username, password: password, firstname: firstname, lastname: lastname }
+                UserModel.create(userdata, function (err, modelResponse) {
+                    if (err) {
+                        res.send(err);
+                    }
+                    else {
+                        var token = jwt.sign({ id: modelResponse._id }, SecretCode, {
+                            expiresIn: 86400 // expires in 24 hours
+                        });
+                        res.json({ 
+                            auth: true, token: token , modelResponse
+                            });
+                    }
+                })
+            }
 
-    })
+        })
 
+    }
+    else {
+        res.send(username + "is not a valid username enter valid username")
+    }
 })
 //-----------------AddProject-----------------//
 
-app.post("/addProject",function(req,res){
+app.post("/addProject", function (req, res) {
     var username = req.body.username;
     var projectname = req.body.projectname;
     var description = req.body.description;
     var shared = req.body.shared;
     console.log(projectdesc);
-    var allprojectdata = {username : username,projectname : projectname ,description : description,shared : shared}
-    dbproject.create(allprojectdata,function(err,dbresponse){
-            res.send(dbresponse);
+    var allprojectdata = { username: username, projectname: projectname, description: description, shared: shared }
+ ProjectModel.create(allprojectdata, function (err, modelResponse) {
+        res.send(modelResponse);
     })
-    
+
 });
 
-var port = 3005;
+//-----------------Update-------------------------//
+app.patch("/edit", verifyToken ,function (req, res) {
+    var updateuserdata = req.body;
+    var username = req.body.username;
+
+    UserModel.findOneAndUpdate({ username: username },
+        { "$set": updateuserdata }).exec(function (err, modelResponse) {
+            if (err) {
+                console.log(err);
+                res.status(500).send(err);
+
+            }
+            if (modelResponse) {
+                UserModel.find({ username: username }, function (err, findResponse) {
+                    if (err) {
+                        res.send(err);
+                    }
+                    else {
+                        res.send(findResponse)
+                    }
+                })
+            }
+        })
+})
+//-----------------searchDetails---------------------//
+app.get("/find", verifyToken ,function (req, res) {
+    
+    var username = req.query.username;
+    UserModel.find({ username: username }, function (err, findResponse) {
+        if (err) {
+            res.send(err);
+        }
+        else {
+            res.send(findResponse)
+        }
+    })
+
+})
+
+
+//-------------------VarifyingTokens----------------------//
+function verifyToken(req, res, next) {
+    var token = req.headers['x-access-token'];
+    if (!token)
+        return res.status(403).send({ auth: false, message: 'No token provided.' });
+        jwt.verify(token, SecretCode, function (err, decoded) {
+        if (err)
+            return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+        // if everything good, save to request for use in other routes
+        req.userId = decoded.id;
+        console.log("passed from varifing function");
+        next();
+    });
+}
+
+
+var port = 3006;
 app.listen(port, function (req, res) {
     console.log("port  " + port + "   in use");
 });
